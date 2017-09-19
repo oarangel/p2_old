38b09e4ae432bf652bd9fb2c1dfa078fe516b243
Oscar Rangel <oarangel6@gmail.com>
2017-09-19 19:50:53 -0400

    Changedt Index File Format

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <!DOCTYPE html>
  <html>
  <head>
-     <title>Dropdown Example</title>
+     <title>Insurance_Calculator</title>
      <meta charset='utf-8'>
      <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
      <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
@@ -10,17 +10,16 @@
  </head>
  <body>
 
-     <h1>Dropdown Example</h1>
+     <h1>Life insurance Premiun Calculator</h1>
+
+     <h2>TO calculate premiun answer the following questions:</h2>
 
      <form method='POST' action='dropdown.php'>
-         <label for='day'>Select which day you're available</label>
-         <select name='day' id='day'>
+         <label for='gender'>Select Gender</label>
+         <select name='gender' id='gender'>
              <option value='choose'>Choose one...</option>
-             <option value='mon' <?php if ($day == 'mon') echo 'SELECTED'?>>Monday</option>
-             <option value='tue' <?php if ($day == 'tue') echo 'SELECTED'?>>Tuesday</option>
-             <option value='wed' <?php if ($day == 'wed') echo 'SELECTED'?>>Wednesday</option>
-             <option value='thu' <?php if ($day == 'thu') echo 'SELECTED'?>>Thursday</option>
-             <option value='fri' <?php if ($day == 'fri') echo 'SELECTED'?>>Friday</option>
+             <option value='fem' <?php if ($gender == 'fem') echo 'SELECTED'?>>Female</option>
+             <option value='male' <?php if ($gender == 'male') echo 'SELECTED'?>>Male</option>
          </select>
 
          <input type='submit' class='btn btn-primary btn-sm'>
@@ -32,10 +31,6 @@
          <?php endif; ?>
 
      </form>
-
-     <footer>
-         <a href='/forms/'>&larr; Other form examples</a>
-     </footer>
 
  </body>
  </html>
